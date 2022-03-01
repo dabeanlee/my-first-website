9069deca7f1c6fc476562dcf2e445b4fa71940e7
Dabean Lee <100480500+dabeanlee@users.noreply.github.com>
2022-03-01 22:41:17 +0900

    Refactoring and fix bug

--- color.js
+++ color.js
@@ -1,33 +1,41 @@
 
+var Links = {
+    setColor:function(color){
+        var alist = document.querySelectorAll('a');
+                        var i = 0;
+                        while(i < alist.length){
+                            alist[i].style.color = color;
+                            i = i + 1
+                        }
+    }
+}            
+var body = {
+    setColor:function(color){
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor:function(color){
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
             function nightdayHandeler(self){
                 
                 var target = document.querySelector('body')
                     if(self.value === 'night'){
-                        target.style.backgroundColor = 'black';
-                        target.style.color = 'white'; 
+                        body.setBackgroundColor('black');
+                        body.setColor('white'); 
                         document.getElementById('grid').style.borderColor = 'white' ;
                         document.getElementById('article').style.borderColor = 'white' ; 
                         self.value = 'day' ;
                         
-                        var alist = document.querySelectorAll('a');
-                        var i = 0;
-                        while(i < alist.length){
-                            alist[i].style.color = 'blue';
-                            i = i + 1
-                        }
-
-                    } else{target.style.backgroundColor = 'white';
-                        target.style.color = 'black';
+                        Links.setColor('powderblue')
+                    } else{body.setBackgroundColor('white');
+                        body.setColor('black');
                         document.getElementById('grid').style.borderColor = 'black' ;
                         document.getElementById('article').style.borderColor = 'black' ; 
                         self.value = 'night' ; 
                         
-                        var alist = document.querySelectorAll('a');
-                        var i = 0;
-                        while(i < alist.length){
-                            alist[i].style.color = 'black';
-                            i = i + 1
-                        }
+                        Links.setColor('black')
+                        
 
                         } 
             }
